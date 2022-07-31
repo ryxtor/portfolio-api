32476a90f0d5a85862714085758062820f40645d
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-07-30 23:15:03 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+skill1 = Skill.create(name: "Ruby")
+skill2 = Skill.create(name: "Rails")
+skill3 = Skill.create(name: "JavaScript")
+skill4 = Skill.create(name: "React")
+skill5 = Skill.create(name: "Redux")
+skill6 = Skill.create(name: "HTML")
+skill7 = Skill.create(name: "CSS")
+skill8 = Skill.create(name: "PostgreSQL")
+skill9 = Skill.create(name: "Bootstrap")
+skill10 = Skill.create(name: "Tailwind")
+
+project1 = Project.new(name: 'Project 1', description: 'This is our first Project', github_link: 'asdasd', live_demo: 'asdasd')
+project1.image.attach(io: File.open('app/assets/images/tree.jpg'), filename: 'tree.jpg')
+project1.skills.push(skill1, skill2, skill3)
+project1.save
